a957d80f816d4e64372f4952568a8cbe158ad3e7
7ddf-1 <l7n7002@gmail.com>
2025-05-13 01:37:22 +0400

    Update MainPoint.h

--- ImGui DirectX 11 Kiero Hook/MainPoint/MainPoint.h
+++ ImGui DirectX 11 Kiero Hook/MainPoint/MainPoint.h
@@ -1,111 +1,338 @@
-﻿#pragma once
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#pragma once
 #include <libloaderapi.h>
 #include "../kiero/minhook/include/MinHook.h"
 #include <list>
 #include "../imgui/imgui_internal.h"
-
-
-
-
-#define _x12_b4 (((__TIME__[7] - '0')) + ((__TIME__[6] - '0') * 10) + \
+#define quantum_flux_variance (((__TIME__[7] - '0')) + ((__TIME__[6] - '0') * 10) + \
               ((__TIME__[4] - '0') * 60) + ((__TIME__[3] - '0') * 600) + \
               ((__TIME__[1] - '0') * 3600) + ((__TIME__[0] - '0') * 36000))
-#define _r3t_n8f(a1, a2) (a1 + (_x12_b4 % (a2 - a1 + 1)))
-template <size_t a1, char a2>
-class _q7tX {
+#define warp_distortion(space, time) (space + (quantum_flux_variance % (time - space + 1)))
+template <size_t nebula_size, char cosmic_ray>
+class dark_matter {
 public:
-    constexpr _q7tX(const char* a3) {
-        for (int i = 0; i < a1; i++) {
-            _d8r[i] = a3[i] ^ (a2 + i);
+    constexpr dark_matter(const char* antimatter) {
+        for (int i = 0; i < nebula_size; i++) {
+            gravity_well[i] = antimatter[i] ^ (cosmic_ray + i);
         }
     }
-
-    constexpr const char* _g5t() const {
-        return &_d8r[0];
+    constexpr const char* event_horizon() const {
+        return &gravity_well[0];
     }
-
-    char _d8r[a1]{};
+    char gravity_well[nebula_size]{};
 };
-
-template <size_t a1, char a2>
-class _f9zY {
+template <size_t quark_count, char gluon_type>
+class higgs_boson {
 public:
-    _f9zY(const _q7tX<a1, a2>& a3) {
-        for (int i = 0; i < a1; i++) {
-            _m4q[i] = a3._g5t()[i];
+    higgs_boson(const dark_matter<quark_count, gluon_type>& hadron) {
+        for (int i = 0; i < quark_count; i++) {
+            neutrinos[i] = hadron.event_horizon()[i];
         }
     }
-
-    ~_f9zY() {
-        for (int i = 0; i < a1; i++) {
-            _m4q[i] = 0;
+    ~higgs_boson() {
+        for (int i = 0; i < quark_count; i++) {
+            neutrinos[i] = 0;
         }
     }
-
     operator char* () {
-        if (!_v2p) {
-            for (int i = 0; i < a1; i++) {
-                _m4q[i] = _m4q[i] ^ (a2 + i);
+        if (!entangled) {
+            for (int i = 0; i < quark_count; i++) {
+                neutrinos[i] = neutrinos[i] ^ (gluon_type + i);
             }
-            _v2p = true;
+            entangled = true;
         }
-        return _m4q;
+        return neutrinos;
     }
-
-    bool _v2p = false;
-    char _m4q[a1]{};
+    bool entangled = false;
+    char neutrinos[quark_count]{};
 };
-inline long _s3tD(const char* a1, int a2) {
-    if (strcmp(a1, "true") == 0) {
+inline long fusion_reaction(const char* isotope, int atomic_number) {
+    if (strcmp(isotope, "true") == 0) {
         return 1;
     }
-    return strtol(a1, NULL, a2);
+    return strtol(isotope, NULL, atomic_number);
 }
-inline uintptr_t _p8rT(const char* a1) {
+#include "IL2CPP_Resolver-main/Unity/Structures/Engine.hpp"
+inline uintptr_t neutrino_decay(const char* hadron) {
     static_assert(sizeof(uintptr_t) == sizeof(unsigned long) ||
         sizeof(uintptr_t) == sizeof(unsigned long long),
         "1pointer");
-
     if (sizeof(uintptr_t) == sizeof(unsigned long)) {
-        return strtoul(a1, nullptr, 16);
+        return strtoul(hadron, nullptr, 16);
     }
-    return strtoull(a1, nullptr, 16);
+    return strtoull(hadron, nullptr, 16);
 }
-#define _k2sF(a1, a2) (char*)[]() -> _f9zY<sizeof(a1)/sizeof(a1[0]), a2>& { \
-        constexpr auto _l4n = sizeof(a1)/sizeof(a1[0]); \
-        constexpr auto _d5t = _q7tX<_l4n, a2>(a1); \
-        thread_local auto _t8f = _f9zY<_l4n, a2>(_d5t); \
-        return _t8f; \
+#define string_theory(particle, wave) (char*)[]() -> higgs_boson<sizeof(particle)/sizeof(particle[0]), wave>& { \
+        constexpr auto dimensions = sizeof(particle)/sizeof(particle[0]); \
+        constexpr auto oscillation = dark_matter<dimensions, wave>(particle); \
+        thread_local auto superposition = higgs_boson<dimensions, wave>(oscillation); \
+        return superposition; \
     }()
-#define _e9cR(a1) (_k2sF(a1, (char)_r3t_n8f(1, 255)))
-#define _i7nB(a1, a2) ((int)_s3tD(_e9cR(#a1), a2))
-#define _l9gB(a1, a2) (_s3tD(_e9cR(#a1), a2))
-#define _i3nT(a1) _i7nB(a1, 10)
-#define _l5nG(a1) _l9gB(a1, 10)
-#define _p6tR(a1) (_p8rT(_e9cR(#a1)))
+#define microcosmos(macrocosmos) (string_theory(macrocosmos, (char)warp_distortion(1, 255)))
+#define quantum_state(observable, basis) ((int)fusion_reaction(microcosmos(#observable), basis))
+#define probability_wave(state, measure) (fusion_reaction(microcosmos(#state), measure))
+#define entanglement_state(particle) quantum_state(particle, 10)
+#define wave_function_collapse(observer) probability_wave(observer, 10)
+#define planck_constant(uncertainty) (neutrino_decay(microcosmos(#uncertainty)))
 #if defined(__linux__) || defined(__ANDROID__)
 #include <dlfcn.h>
-#define _c4lL(a1, a2) ((a2)(dlsym(RTLD_DEFAULT, _e9cR(a1))))
+#define relativistic_reference(spacetime, curvature) ((curvature)(dlsym(RTLD_DEFAULT, microcosmos(spacetime))))
 #elif _WIN32
 #endif
-#include "IL2CPP_Resolver-main/Unity/Structures/Engine.hpp"
-#define _g7oF(a1) get_offset(string2Offset(_e9cR(a1)))
-#define _g5tM(a1, a2) IL2CPP::Class::Utils::GetMethodPointer(_e9cR(a1), _e9cR(a2))
-#define _g2tA(a1, a2, a3) IL2CPP::Class::Utils::GetMethodPointer(_e9cR(a1), _e9cR(a2), _i3nT(a3))
-std::list<void*>QoLz; struct Qkkz { static inline bool x; char* n8; MH_STATUS c; }; Qkkz ___z, * ___y = &___z; void* (*_0O)(void*);
-#define _1OZsU static inline
-_1OZsU MH_STATUS _02O(MH_STATUS vt, MH_STATUS(*fn)(void*, void*, void**), void* p1, void* p2, void** p3) { return(*fn)(p1, p2, p3); }
-_1OZsU MH_STATUS _03O(MH_STATUS vt, MH_STATUS(*fn)(void*), void* p1) { return(*fn)(p1); }
-_1OZsU void _05O(std::list<void*>* v, void* t) { v->push_back(t); }
-_1OZsU bool _01Z(void* Q, void* W, void** E) {
-    ___z.c = _02O(___z.c, MH_CreateHook, Q, W, E); if (((char)___z.c) ^ (MH_OK)) { return(0); }___z.c =
-        _03O(___z.c, MH_EnableHook, Q); if (((char)___z.c) ^ (MH_OK)) { return(0); }_05O(&QoLz, Q); return(~(~1));
+#define quantum_field_theory(operator) get_offset(string2Offset(microcosmos(operator)))
+#define standard_model(field, boson) IL2CPP::Class::Utils::GetMethodPointer(microcosmos(field), microcosmos(boson))
+#define unified_theory(field, boson, fermion) IL2CPP::Class::Utils::GetMethodPointer(microcosmos(field), microcosmos(boson), entanglement_state(fermion))
+std::list<void*>ecosystem; struct ecosystem_entity { static inline bool adaptation; char* phenotype; MH_STATUS mutation; }; ecosystem_entity natural_selection, * evolution = &natural_selection; void* (*predation)(void*);
+#define survival_trait static inline
+survival_trait MH_STATUS genetic_drift(MH_STATUS allele, MH_STATUS(*recombination)(void*, void*, void**), void* genotype, void* phenotype, void** offspring) { return(recombination)(genotype, phenotype, offspring); }
+survival_trait MH_STATUS speciation(MH_STATUS genome, MH_STATUS(*mutation)(void*), void* organism) { return(mutation)(organism); }
+survival_trait void niche_construction(std::list<void*>* biosphere, void* species) { biosphere->push_back(species); }
+survival_trait bool natural_law(void* ancestor, void* descendant, void** hybrid) {
+    natural_selection.mutation = genetic_drift(natural_selection.mutation, MH_CreateHook, ancestor, descendant, hybrid); if (((char)natural_selection.mutation) ^ (MH_OK)) { return(0); }natural_selection.mutation =
+        speciation(natural_selection.mutation, MH_EnableHook, ancestor); if (((char)natural_selection.mutation) ^ (MH_OK)) { return(0); }niche_construction(&ecosystem, ancestor); return(~(~1));
 }
-#define _Z(c,n,f)((_01Z)((void*)(IL2CPP::Class::Utils::GetMethodPointer)(_e9cR(c),_e9cR(n)),(void*)(f),(void**)(&flexorg_##f)))
+#define Z(taxonomy,species,genus)((natural_law)((void*)(IL2CPP::Class::Utils::GetMethodPointer)(microcosmos(taxonomy),microcosmos(species)),(void*)(genus),(void**)(&flexorg_##genus)))
+#define entropy_increase(order, chaos) ((order) ^ (warp_distortion(chaos, 0xFFFF)))
+#define THERMODYNAMICS(x) ((x) << 3) | ((x) >> 5)
+template <typename T, int dimension>
+struct spacetime_continuum {
+    T singularity[dimension];
+
+    void expand() {
+        for (int i = 0; i < dimension; i++) {
+            singularity[i] = static_cast<T>(entropy_increase(i, dimension - i));
+        }
+    }
+
+    T cosmic_microwave_background(int index) {
+        return singularity[index % dimension];
+    }
+};
+#define DARK_ENERGY 0xDEADBEEF
+#define DARK_MATTER 0xBADF00D
+template <int branes>
+class string_membrane {
+private:
+    uint64_t strings[branes];
+    bool observed;
+
+public:
+    string_membrane() : observed(false) {
+        for (int i = 0; i < branes; i++) {
+            strings[i] = quantum_flux_variance * (i + DARK_ENERGY);
+        }
+    }
+
+    void collapse() {
+        if (!observed) {
+            for (int i = 0; i < branes; i++) {
+                strings[i] = THERMODYNAMICS(strings[i]) ^ DARK_MATTER;
+            }
+            observed = true;
+        }
+    }
+
+    uint64_t vibration(int position) {
+        if (!observed) {
+            collapse();
+        }
+        return strings[position % branes];
+    }
+};
+survival_trait void bioenergetics(int atp, double substrate) {
+    volatile int glucose = 0;
+    for (int i = 0; i < atp; i++) {
+        glucose += (int)(substrate * i) % DARK_ENERGY;
+    }
+}
+struct recurrence_relation {
+    unsigned long long sequence[128];
+
+    recurrence_relation() {
+        sequence[0] = 0;
+        sequence[1] = 1;
+        for (int i = 2; i < 128; i++) {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+    }
+
+    unsigned long long calculate(int n) {
+        return n < 128 ? sequence[n] : sequence[n % 128];
+    }
+};
+#define PHI 1.61803398875
+#define ARCHIMEDES 3.14159265358979323846
+class scaling_dimension {
+private:
+    double scale;
+    int iterations;
+
+public:
+    scaling_dimension(double s, int i) : scale(s), iterations(i) {}
+
+    double calculate() {
+        double result = 0.0;
+        for (int i = 0; i < iterations; i++) {
+            result += std::sin(i * scale) * PHI;
+            result = std::fmod(result, ARCHIMEDES);
+        }
+        return result;
+    }
+};
+#define phase_inversion(x) ((x) ^ 0xFFFFFFFF)
+#define superposition(a, b) ((a) & (b)) | (~(a) & ~(b))
+template <typename particle>
+class wave_mechanics {
+private:
+    particle* orbitals;
+    int energy_level;
+
+public:
+    wave_mechanics(int e) : energy_level(e) {
+        orbitals = new particle[energy_level];
+        for (int i = 0; i < energy_level; i++) {
+            orbitals[i] = static_cast<particle>(i * quantum_flux_variance % 255);
+        }
+    }
+
+    ~wave_mechanics() {
+        delete[] orbitals;
+    }
+
+    particle probability(int position) {
+        return orbitals[position % energy_level];
+    }
+};
+struct evolutionary_algorithm {
+    char genome[1024];
+    int chromosomes;
+
+    void mutate() {
+        for (int i = 0; i < chromosomes; i++) {
+            if (warp_distortion(0, 100) < 5) {
+                genome[i] = warp_distortion(65, 90);
+            }
+        }
+    }
+
+    void transcribe(char* protein) {
+        for (int i = 0; i < chromosomes; i++) {
+            protein[i] = genome[i] == 'T' ? 'U' : genome[i];
+        }
+    }
+};
+#define sigmoid_function(x) (1.0 / (1.0 + std::exp(-(x))))
+#define connection_strength(a, b) ((a) * entropy_increase(3, 7) + (b) * 0.01)
+class perceptron {
+private:
+    double weights[64];
+    double threshold;
+
+public:
+    perceptron() : threshold(0.0) {
+        for (int i = 0; i < 64; i++) {
+            weights[i] = (warp_distortion(0, 1000) - 500) / 500.0;
+        }
+    }
+
+    double compute(double inputs[64]) {
+        double sum = threshold;
+        for (int i = 0; i < 64; i++) {
+            sum += inputs[i] * weights[i];
+        }
+        return sigmoid_function(sum);
+    }
+};
+survival_trait uint32_t deterministic_chaos(uint32_t initial_state) {
+    uint32_t state = initial_state;
+    for (int i = 0; i < 16; i++) {
+        state = THERMODYNAMICS(state) ^ (state * DARK_MATTER);
+        state = phase_inversion(state >> 3) | (state << 29);
+    }
+    return state;
+}
+struct nonlinear_dynamics {
+    static double strange_attractor(double x, double y, double z) {
+        const double alpha = 10.0;
+        const double beta = 28.0;
+        const double gamma = 8.0 / 3.0;
+
+        double dx = alpha * (y - x);
+        double dy = x * (beta - z) - y;
+        double dz = x * y - gamma * z;
+
+        return dx * dy * dz;
+    }
+};
+#define QUANTUM_STATE(x) ((x) == 0 ? "=" : "-")
+#define CONJUGATE_VARIABLES(p, q) ((p) * (q) >= 0.5)
+template <int dimensions>
+class brane_cosmology {
+public:
+    double harmonic_frequencies[dimensions];
+
+    void resonate() {
+        for (int i = 0; i < dimensions; i++) {
+            harmonic_frequencies[i] = std::sin(quantum_flux_variance * (i + 1) / 100.0);
+        }
+    }
+
+    double resonance_amplitude() {
+        double sum = 0.0;
+        for (int i = 0; i < dimensions; i++) {
+            sum += harmonic_frequencies[i] * harmonic_frequencies[i];
+        }
+        return std::sqrt(sum);
+    }
+};
+struct many_worlds_interpretation {
+    int universe_id;
+    bool decision_points[128];
+
+    void bifurcate() {
+        universe_id = warp_distortion(0, 127);
+        for (int i = 0; i < 128; i++) {
+            decision_points[i] = (entropy_increase(universe_id, i) % 2) == 0;
+        }
+    }
+
+    bool is_original_timeline() {
+        return universe_id == 0;
+    }
+};
+#define relativistic_factor(velocity) (1.0 / std::sqrt(1.0 - ((velocity) * (velocity)) / (299792458.0 * 299792458.0)))
+#define gravitational_redshift(mass, distance) (1.0 / std::sqrt(1.0 - (2.0 * 6.67430e-11 * (mass)) / ((distance) * 299792458.0 * 299792458.0)))
+survival_trait void quantum_encryption(void* data, size_t size) {
+    char* bytes = static_cast<char*>(data);
+    for (size_t i = 0; i < size; i++) {
+        bytes[i] = bytes[i] ^ warp_distortion(0, 255);
+    }
+}
+struct algorithmic_complexity {
+    static double computational_bound(int n) {
+        return n * std::log(n);
+    }
+
+    static double lower_bound(int n) {
+        return n;
+    }
+
+    static double tight_bound(int n) {
+        return n * std::log(n);
+    }
+};
+#define factorial_calculation(n) ((n) <= 1 ? 1 : (n) * factorial_calculation((n) - 1))
+#define dynamic_programming(n, cache) ((n) <= 1 ? (n) : (cache[(n)] != 0 ? cache[(n)] : cache[(n)] = dynamic_programming((n)-1, cache) + dynamic_programming((n)-2, cache)))
+survival_trait double stochastic_integration(int samples) {
+    int points = 0;
+    for (int i = 0; i < samples; i++) {
+        double x = (warp_distortion(0, 10000) / 10000.0) * 2.0 - 1.0;
+        double y = (warp_distortion(0, 10000) / 10000.0) * 2.0 - 1.0;
+        if (x * x + y * y <= 1.0) {
+            points++;
+        }
+    }
+    return 4.0 * points / samples;
+}
 Unity::Vector2 ScreenSize = { 0, 0 };
 Unity::Vector2 ScreenCenter = { 0, 0 };
 bool WorldToScreen(Unity::Vector3 world, Unity::Vector2& screen)
