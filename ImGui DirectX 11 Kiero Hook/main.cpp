eb6fe72a872a322ce82a4aa5f53b840ceedb4d33
Luke <137133945+LukeTheNuke64@users.noreply.github.com>
2025-05-11 17:02:04 -0700

    Update main.cpp

--- ImGui DirectX 11 Kiero Hook/main.cpp
+++ ImGui DirectX 11 Kiero Hook/main.cpp
@@ -1,5 +1,6 @@
 #include "includes.h"
 #include "AltPoint/Drawing.h"
+
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 Present oPresent;
 HWND window = NULL;
@@ -7,6 +8,8 @@ WNDPROC oWndProc;
 ID3D11Device* pDevice = NULL;
 ID3D11DeviceContext* pContext = NULL;
 ID3D11RenderTargetView* mainRenderTargetView;
+bool showMenu = false;
+
 void InitImGui()
 {
 	ImGui::CreateContext();
@@ -16,11 +19,10 @@ void InitImGui()
 	ImGui_ImplDX11_Init(pDevice, pContext);
 }
 
-LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-
-	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (showMenu && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
 		return true;
-
 	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
 }
 
@@ -29,92 +31,66 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 {
 	if (!init)
 	{
-		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)& pDevice)))
+		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&pDevice)))
 		{
 			pDevice->GetImmediateContext(&pContext);
 			DXGI_SWAP_CHAIN_DESC sd;
 			pSwapChain->GetDesc(&sd);
 			window = sd.OutputWindow;
+
 			ID3D11Texture2D* pBackBuffer;
-			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer);
+			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
 			pDevice->CreateRenderTargetView(pBackBuffer, NULL, &mainRenderTargetView);
 			pBackBuffer->Release();
+
 			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
 			InitImGui();
 			init = true;
 		}
-
 		else
+		{
 			return oPresent(pSwapChain, SyncInterval, Flags);
+		}
 	}
 
-
-	ImGui_ImplDX11_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-	ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_Always);
-	if (ImGui::Begin("##mainthing", nullptr, winflags))
+	static bool togglePressed = false;
+	if (GetAsyncKeyState(VK_INSERT) & 0x8000)
 	{
-		ImGui::Text("Pixel Strike Base By 0SD47");
-		ImGui::Separator();
-		ImGui::Spacing();
-		ImGui::Checkbox("No Recoil", &norecoil);
-
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Mass Loop Reload", &MassLoopReload);
-
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Speed Test", &SpeedTest);
-		ImGui::SameLine();
-		ImGui::Text("[Bannable]");
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Jump Boost", &JumpBoost);
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Fly", &Fly);
-
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Infinite Jump", &InfiniteJump);
-
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Rapid Fire", &RapidFire);
-
-		ImGui::Spacing();
-
-		ImGui::Checkbox("Set Weapon", &SetWeapon);
-
-		ImGui::Spacing();
-
-		ImGui::Spacing();
-
-		if (SpeedTest)
+		if (!togglePressed)
 		{
-			ImGui::SliderFloat("Speed", &SpeedTestSlider, 0.f, 120.f);
+			showMenu = !showMenu;
+			togglePressed = true;
 		}
-
-
-		if (ImGui::Button("Get Reward"))
-		{
-			GetReward = true;
-		}
-		else
-			GetReward = false;
-
-
-		ImGui::End();
 	}
-	ImGui::Render();
+	else
+	{
+		togglePressed = false;
+	}
 
-	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
-	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	if (showMenu)
+	{
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+		ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_Always);
+		if (ImGui::Begin("##mainthing", nullptr, winflags))
+		{
+			ImGui::Text("Pixel Strike 3D Cheat");
+			ImGui::Separator();
+			ImGui::Spacing();
+			ImGui::Checkbox("No Recoil", &norecoil);
+			ImGui::Checkbox("Infinite ammo", &ammoEnabled);
+			ImGui::Separator();
+			ImGui::Text("Credit to OSD47 for cheat base");
+			ImGui::End();
+		}
+		ImGui::Render();
+		pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	}
+
 	return oPresent(pSwapChain, SyncInterval, Flags);
 }
-
 
 bool Hooks()
 {
@@ -129,33 +105,23 @@ bool Hooks()
 	}
 	return true;
 }
+
 uintptr_t Base = (uintptr_t)GetModuleHandleW(L"GameAssembly.dll");
 DWORD pid = GetCurrentProcessId();
-
 
 void safecall()
 {
 	AllocConsole();
 	FILE* fp;
-	freopen_s(&fp, "CONOUT$", "w", stdout);;
-	SetConsoleTitleA("Pixel Strike Base By 0SD47");
-	const HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+	SetConsoleTitleA("Pixel Strike 3D Cheat");
 
 	printf("Base address: 0x%p\n", (void*)Base);
 	printf("Process ID: %lu\n", pid);
 	IL2CPP::Initialize();
 	Hooks();
-
-
+	printf("Menu open bind: INSERT\n");
 }
-
-
-
-
-
-
-
-
 
 DWORD WINAPI MainThread(LPVOID lpReserved)
 {
@@ -171,7 +137,6 @@ DWORD WINAPI MainThread(LPVOID lpReserved)
 	} while (!init_hook);
 	return TRUE;
 }
-
 
 BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
 {
